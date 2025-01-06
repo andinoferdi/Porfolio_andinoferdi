99bf0079d5dfeb21248d7bb54b9bf884ec1d5c4c
AndinoFerdi <andinoferdiansah@gmail.com>
2025-01-06 22:57:54 +0700

    Update

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,36 @@
-// import images
-import Header_person from "./assets/images/Header/Foto.jpg";
+// Header assets
+import Header_person from "/src/assets/images/Header/Foto.jpg";
 
-import figma from "./assets/images/Skills/figma.png";
-import sketch from "./assets/images/Skills/sketch.png";
-import ps from "./assets/images/Skills/ps.png";
-import reactjs from "./assets/images/Skills/react.png";
-import nodejs from "./assets/images/Skills/node.png";
-import python from "./assets/images/Skills/python.png";
+// Skills assets
+import figma from "/src/assets/images/Skills/figma.png";
+import sketch from "/src/assets/images/Skills/sketch.png";
+import ps from "/src/assets/images/Skills/ps.png";
+import reactjs from "/src/assets/images/Skills/react.png";
+import nodejs from "/src/assets/images/Skills/node.png";
+import python from "/src/assets/images/Skills/python.png";
 
-import services_logo1 from "./assets/images/Services/logo1.png";
-import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
+// Services assets
+import services_logo1 from "/src/assets/images/Services/logo1.png";
+import services_logo2 from "/src/assets/images/Services/logo2.png";
+import services_logo3 from "/src/assets/images/Services/logo3.png";
 
+// Projects assets
 import project1 from "/src/assets/images/Projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project2 from "/src/assets/images/Projects/img2.png";
+import project3 from "/src/assets/images/Projects/img3.png";
+import person_project from "/src/assets/images/Projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+// Testimonials assets
+import avatar1 from "/src/assets/images/Testimonials/avatar1.png";
+import avatar2 from "/src/assets/images/Testimonials/avatar2.png";
+import avatar3 from "/src/assets/images/Testimonials/avatar3.png";
+import avatar4 from "/src/assets/images/Testimonials/avatar4.png";
 
-import Hireme_person from "./assets/images/Hireme/person.png";
-import Hireme_person2 from "./assets/images/Hireme/person2.png";
+// Hireme assets
+import Hireme_person from "/src/assets/images/Hireme/person.png";
+import Hireme_person2 from "/src/assets/images/Hireme/person2.png";
 
+// React Icons
 import { GrMail } from "react-icons/gr";
 import { MdArrowForward, MdCall } from "react-icons/md";
 import { BsInstagram } from "react-icons/bs";
